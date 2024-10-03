ffda01aaf49e2e1b1b9e7846ae115474f14a6bbe
Randy <dddrandy@gmail.com>
2024-10-03 17:06:14 +0930

    Fix votes for comment and poi.

--- src/models/vote.js
+++ src/models/vote.js
@@ -5,7 +5,7 @@ export const MODEL_VOTE = 'Vote';
 const VoteSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: MODEL_USER, required: true },
   targetId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  targetType: { type: String, enum: ['Question', 'Answer'], required: true },
+  targetType: { type: String, required: true },
   voteType: { type: String, enum: ['upvote', 'downvote'], required: true },
 }, { timestamps: true });
 
