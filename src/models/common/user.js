dd83f512698185c447ddfaeeacfd70c2e0dba23c
Randy <dddrandy@gmail.com>
2024-10-04 10:01:01 +0930

    Fix username unique error.

--- src/models/common/user.js
+++ src/models/common/user.js
@@ -3,7 +3,7 @@ import { IMAGES_FILES } from '../../gridfs.js';
 
 export const MODEL_USER = 'common.User';
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
   phone: { type: String },
